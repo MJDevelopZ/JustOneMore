8adcde05da21f6729ff42fefb2355f9a13aa8cb8
MJDevelopZ <mjdevelopz@gmail.com>
2015-12-22 11:29:14 +0300

    Refactored code to use collect

--- LevelOne.rb
+++ LevelOne.rb
@@ -1,11 +1,14 @@
-#Need to loop through an Array
+#Need to loop through an Array adding one to each element in the array.
 
 #Beginning Array
 begArray=[1,0,-1,5,100,37,20,18,12,0]
 
 #Iterate through begArray
-for i in 0..begArray.length-1
+#for i in 0..begArray.length-1
 	#add 1 to all the elements
-	puts (begArray[i] + 1)
-end
+	#puts (begArray[i] + 1)
+#end
 
+#Refactored to use collect
+result = begArray.collect{|e| e + 1}
+p result
