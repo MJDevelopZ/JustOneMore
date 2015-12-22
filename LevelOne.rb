c6abf204668ec1e1a2ff5c19b727dab418be6932
MJDevelopZ <mjdevelopz@gmail.com>
2015-12-22 09:09:24 +0300

    Level One Solution

---
+++ LevelOne.rb
@@ -0,0 +1,11 @@
+#Need to loop through an Array
+
+#Beginning Array
+begArray=[1,0,-1,5,100,37,20,18,12,0]
+
+#Iterate through begArray
+for i in 0..begArray.length-1
+	#add 1 to all the elements
+	puts (begArray[i] + 1)
+end
+
